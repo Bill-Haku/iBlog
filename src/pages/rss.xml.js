d42a72e2d3601dfe23cae2498b1f8bcc78174dee
Bill-Haku <battleandwar@hotmail.com>
2023-10-28 19:36:04 +0800

    update images

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: "Austin's Blog",
-    description: "Site description",
-    site: 'https://astro-blog.qum.cc',
+    title: "Bill Haku's Blog",
+    description: "Collection of Bill Haku's blogs",
+    site: 'https://blog.hakubill.tech',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>zh-cn</language>`,
   });
