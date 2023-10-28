9d367a2ac9fed419ece10cbe6dd1864aa1f8d0e5
Bill-Haku <battleandwar@hotmail.com>
2023-10-28 13:23:05 +0800

    update new texts

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro-blog.qum.cc';
-export const SITE_URL = "https://astro-blog.qum.cc";
+export const SITE_TITLE = `Bill Haku's Blog`;
+export const SITE_DESCRIPTION = 'Bill Haku的个人文章站';
+export const SITE_EMAIL = 'i@hakubill.tech';
+export const SITE_NAME = 'blog.hakubill.tech';
+export const SITE_URL = "https://blog.hakubill.tech";
---
+++ src/pages/posts/japan_s1e1.md
@@ -0,0 +1,14 @@
+---
+layout: '../../layouts/MarkdownPost.astro'
+title: '回乡记-东航A350商务舱体验'
+pubDate: 2023-10-28
+description: 'Apple 与 Applied Environmental Research Foundation 合作，将促进印度马哈拉施特拉邦红树林的保育工作'
+author: 'Bill Haku'
+cover:
+    url: 'https://blog.hakubill.tech/img/jp/_DSC0093.heic'
+    square: 'https://blog.hakubill.tech/img/jp/_DSC0093.heic'
+    alt: 'cover'
+tags: ["日本", "杂谈", "旅游"]
+theme: 'dark'
+featured: true
+---
