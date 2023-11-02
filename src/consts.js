e8e47cc0c499f052c9afc72251a5ea12b09925c5
Bill-Haku <battleandwar@hotmail.com>
2023-11-02 15:22:33 +0800

    update post

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-export const SITE_TITLE = `Bill Haku's Blog`;
+export const SITE_TITLE = `iBlog of Bill Haku`;
 export const SITE_DESCRIPTION = 'Bill Haku的个人文章站';
 export const SITE_EMAIL = 'i@hakubill.tech';
 export const SITE_NAME = 'blog.hakubill.tech';
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: "Bill Haku's Blog",
+    title: "iBlog of Bill Haku",
     description: "Collection of Bill Haku's blogs",
     site: 'https://blog.hakubill.tech',
     items: await pagesGlobToRssItems(import.meta.glob('./posts/*.md')),
